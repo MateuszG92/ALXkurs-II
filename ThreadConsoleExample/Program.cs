10e56662c05baf399f7de56349d31101be29e0d4
MateuszG92 <mateuszg00@gmail.com>
2023-03-18 10:17:26 +0100

    Add project files.

---
+++ ThreadConsoleExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadConsoleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LongOeration(); synchronic
+
+            Thread thread1 = new Thread(new ThreadStart(LongOeration));
+            thread1.Priority=ThreadPriority.Normal;
+            thread1.Start();
+
+            Thread thread2 = new Thread(()=>LongOeration());
+            thread2.Start();
+
+            thread1.Abort();
+
+            thread1.Join();
+            thread2.Join(); 
+
+            Console.WriteLine("Koniec pracy");
+            Console.ReadKey();
+        }
+
+        static void LongOeration()
+        {
+            int threatId=Thread.CurrentThread.ManagedThreadId;
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine($"{threatId} counter : {i}");
+            }
+        }
+    }
+}
