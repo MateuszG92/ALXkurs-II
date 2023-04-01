a8431bb2d9f1bae8a61215b3e45835ee573ebf21
MateuszG92 <mateuszg00@gmail.com>
2023-04-01 15:18:01 +0200

    new functionalities

--- WPFTest/TextBlockExample.xaml.cs
+++ WPFTest/TextBlockExample.xaml.cs
@@ -22,11 +22,24 @@ namespace WPFTest
         public TextBlockExample()
         {
             InitializeComponent();
+            textBlockFormat.Inlines.Clear();
+            textBlockFormat.Inlines.Add("aaa ");
+            textBlockFormat.Inlines.Add(new Run("BBB ")
+            {
+                FontSize = 20,
+                FontWeight = FontWeights.Bold
+            });
+            textBlockFormat.Inlines.Add(new Run("CCC")
+            {
+                Foreground = Brushes.Aquamarine,
+                TextDecorations = TextDecorations.Strikethrough
+            });
         }
 
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
             System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
         }
+
     }
 }
