8abc795ab3f4b3e44258f1e4b4de5ddaba9c6e1f
MateuszG92 <mateuszg00@gmail.com>
2023-04-22 16:52:42 +0200

    day5

---
+++ WebApiTest1/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTest1.Models;
+
+namespace WebApiTest1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly List<Products> products = new List<Products>
+        {
+            new Products { Id = 1,Name="Mleko",Description="opis mleka", Price=4.56M},
+            new Products { Id = 2,Name="Ryż", Description="ryż opis", Price=2.34M}
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Products>> Get()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Products> Get(int id)
+        {
+            var product = products.Find(x => x.Id == id);
+            if (product == null) { return NotFound(); }
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult<Products> Post(Products newProduct)
+        {
+            products.Add(newProduct);
+            return CreatedAtAction(nameof(Get), new { id = newProduct.Id }, newProduct);
+            //return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var product = products.Find(x => x.Id == id);
+            if (product == null) { return NotFound(); }
+            products.Remove(product);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, Products upProduct)
+        {
+            var product = products.Find(x => x.Id == id);
+            if (product == null) { return NotFound(); }
+            product.Name=upProduct.Name;
+            product.Price = upProduct.Price;
+            product.Description=upProduct.Description;
+            return NoContent();
+        }
+    }
+}
---
+++ WebApiTest1/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTest1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id) {  return "value"+id; }
+
+        [HttpPost]
+       // public string Post(string value) 
+        public string Post([FromBody] string value) 
+        {
+            return $"POST: {value}";
+        }   
+    }
+}
